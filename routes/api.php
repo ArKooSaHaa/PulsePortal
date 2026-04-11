db6e010ac4cc112f3a4849d3e1079647bb37d764
Shahadat Hasan <tamimshahadat15@gmail.com>
2026-04-11 22:01:13 +0600

    Add refresh

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login',    [AuthController::class, 'login']);
     Route::post('logout',   [AuthController::class, 'logout'])->middleware('auth:api');
+    Route::post('refresh',  [AuthController::class, 'refresh'])->middleware('auth:api');
     Route::get('me',        [AuthController::class, 'me'])->middleware('auth:api');
 
     // Google OAuth
